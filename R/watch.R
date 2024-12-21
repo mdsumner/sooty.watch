8a7a7e7f996c9766095790554faeb5fe8021a852
Michael Sumner <mdsumner@gmail.com>
2024-12-22 09:26:58 +1100

    namespace

--- R/watch.R
+++ R/watch.R
@@ -1,9 +1,9 @@
-
 #' Watch the new online data library
 #'
 #' @return data frame with summary of curated datasets
 #' @export
-#'
+#' @name watch
+#' @aliases watch_buckets watch_curated
 #' @examples
 #' watch_curated()
 watch_curated <- function() {
@@ -14,7 +14,8 @@ watch_curated <- function() {
   dplyr::arrange(desc(maxdate))
 }
 
-
+#' @name watch
+#' @export
 watch_buckets <- function() {
   dplyr::group_by(sooty:::.objects() , .data$Bucket) |>
   dplyr::summarize(n = dplyr::n()) |>
